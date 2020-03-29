0d6b8c8ba20f2f964c7dd383cd77f31635ecc66b
artem-vasiltsov <artemvasiltsov01@gmail.com>
2020-03-29 09:23:35 +0300

    Modified, Add counter, tracking

--- app.py
+++ app.py
@@ -3,5 +3,4 @@ from src.object_counter.beverage_counter import BeverageCounter
 
 if __name__ == '__main__':
 
-    BeverageCounter().main(video_path="/media/mensa/Data/Task/TrolleyDetection/"
-                                      "How to Sip a Soda, Like a Sir _ FOODBEAST LABS.mp4")
+    BeverageCounter().main(video_path="")
